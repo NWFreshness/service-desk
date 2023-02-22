69ef2f9d7143a94f94e5a523886b99ecbaf9aa5c
Tyler Mayfield <tyler.mayfield23@gmail.com>
2023-02-21 16:10:40 -0800

    finished up backend note functionality

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {protect} = require('../middleware/authMiddleware');
+
+// Re-route into note router
+const noteRouter = require('./noteRoutes');
+router.use('/:ticketId/notes', noteRouter);
+
 const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
